cf23264bd8939c010a2eb32f3162b63a8b467cf0
ShookEagle <shookeagle@gmail.com>
2024-07-28 12:11:04 -0500

    Change teamnames in scoreboard

--- OsVSNOs.cs
+++ OsVSNOs.cs
@@ -20,6 +20,14 @@ public class OsVSNOs : BasePlugin
         AddCommandListener("jointeam", Command_Jointeam);
         OTeam = CsTeam.Terrorist;
         NOTeam = CsTeam.CounterTerrorist;
+        Server.ExecuteCommand($"mp_teamname_1 NO's");
+        Server.ExecuteCommand($"mp_teamname_2 O's");
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        Server.ExecuteCommand("mp_teamname_1 COUNTER-TERRORISTS");
+        Server.ExecuteCommand("mp_teamname_2 TERRORISTS");
     }
 
     public HookResult Command_Jointeam(CCSPlayerController? player, CommandInfo info)
@@ -37,6 +45,9 @@ public class OsVSNOs : BasePlugin
         CsTeam temp = OTeam;
         OTeam = NOTeam;
         NOTeam = temp;
+
+        Server.ExecuteCommand($"mp_teamname_1 {(OTeam == CsTeam.CounterTerrorist ? "O's" : "NO's")}");
+        Server.ExecuteCommand($"mp_teamname_2 {(OTeam == CsTeam.Terrorist ? "O's" : "NO's")}");
 
         var OTags = Utilities.GetPlayers().Where(p => p.IsOTag());
         var NOTags = Utilities.GetPlayers().Where(p => !p.IsOTag());
